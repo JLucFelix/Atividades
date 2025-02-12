3a0a28476f713452001569455f15d65f95481c25
JLucFelix <120970396+JLucFelix@users.noreply.github.com>
2025-02-12 20:46:00 -0300

    api_dia12.php

--- api.php
+++ api.php
@@ -21,9 +21,62 @@ Route::get ('/contagem_jean', function(){
 });
 
 Route::get ('/contagem_primos', function(){
-    for ($i=1; $i<=100; $i++){
-        if($i / $i == 0){
-            echo $i, "\n";
+    for ($i=2; $i<=100; $i++){
+        $primo = true;
+        for($j = 2; $j<$i; $j++){
+            if($i%$j == 0 ){
+                $primo = false;
+                break;
+            }
+        }
+        if($primo){
+            echo $i . "\n";
         }
     }
+});
+
+Route::get('/users/{id}', function($id){
+    return $id;
+});
+
+Route::get('contagem_rota/{contagem}', function($contagem){
+    for ($i =0; $i<$contagem + 1; $i++){
+        echo $i . "\n";
+    }
+});
+
+Route::get('par_impar/{numero}', function($numero){
+    if($numero%2==0){
+        return 'Par';
+    }
+    else{
+        return 'Impar';
+    }
+});
+
+Route::get('primo/{numeroprimo}', function($numeroprimo){
+    $resultado = 0;
+    for ($i=2; $i<$numeroprimo; $i++){
+        if ($numeroprimo % $i == 0){
+            $resultado++;
+            break;
+        } 
+    }
+    if ($resultado == 0){
+        echo 'É primo';
+    }
+    else {
+        echo 'Não é primo';
+    }
+});
+
+Route::get('contagem/{numero1}/{numero2}', function($numero1, $numero2){
+    for ($i =$numero1; $i<$numero2; $i++){
+        echo $i . "\n";
+    }
+});
+
+Route::get('calculadora/{numero1}/{numero2}',function($numero1,$numero2){
+    $resultado = $numero1 + $numero2;
+    echo 'O resultado é ', $resultado;  
 });
